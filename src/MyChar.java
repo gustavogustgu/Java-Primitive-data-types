d80837b3436432dffd775fb541c7d4990ed513fc
Gustavo Oliveira <gustavogustgu@gmail.com>
2023-09-06 12:09:15 -0300

    imprimir as letras

--- src/MyChar.java
+++ src/MyChar.java
@@ -5,22 +5,43 @@ public class MyChar {
         this.ch = ch;
     }
 
-    /*public static void printUpperCaseAlphabets() {
+    public static void printUpperCaseAlphabets() {
+        for (char ch = 'a'; ch <= 'z'; ch++){
+            System.out.println(ch);
+        }
     }
 
     public static void printLowerCaseAlphabets() {
+        for (char ch = 'A'; ch <= 'Z'; ch++){
+            System.out.println(ch);
+        }
     }
 
-    public boolean isNumber() {
+    public boolean isDigit() {
+        if(ch >= 48 && ch <=57) //between '0' and '9'
+            return true;
+        return false;
     }
 
     public boolean isAlphabet() {
-    }*/
+        if(ch >= 97 && ch <=122)// between 'a' and 'z'
+            return true;
+
+        if(ch >= 65 && ch <=90)// between 'A' and 'Z'
+            return true;
+        return false;
+    }
 
     public boolean isVowel() {
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
                 || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-        return true;
+            return true;
+        return false;
+    }
+
+    public boolean isConsonant() {
+        if(isAlphabet() && !isVowel())
+            return true;
         return false;
     }
 }
--- src/MyCharRunner.java
+++ src/MyCharRunner.java
@@ -1,11 +1,14 @@
 public class MyCharRunner {
     public static void main(String[] args) {
 
-        MyChar myChar = new MyChar('a');
+        MyChar myChar = new MyChar('B');
+
         System.out.println(myChar.isVowel());
-        /*System.out.println(myChar.isNumber());
+        System.out.println(myChar.isDigit());
         System.out.println(myChar.isAlphabet());
+
+        System.out.println(myChar.isConsonant());
         MyChar.printLowerCaseAlphabets();
-        MyChar.printUpperCaseAlphabets();*/
+        MyChar.printUpperCaseAlphabets();
     }
 }
